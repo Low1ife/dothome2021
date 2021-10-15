6a128a4d1b63cc8c191a9e5f78622a14441eeef0
kimtaehwi84 <87642334+wow2002r@users.noreply.github.com>
2021-10-15 16:48:02 +0900

    Create sound.php

---
+++ TEAM-WORK/goodnight/sound/sound.php
@@ -0,0 +1,77 @@
+<?php
+    include "../connect/connect.php";
+    include "../connect/session.php";
+?>
+
+<?php
+    $title = "굿밤 | 커뮤니티";
+    include "../include/head.php"; 
+?>
+<body>
+
+    <div id="skip">
+        <a href="#contents" class="ir_so">컨텐츠 바로가기</a>
+        <a href="#footer" class="ir_so">푸터 바로가기</a>
+    </div>
+    <!-- //skip -->
+    
+        <?php
+            $title = "굿밤 | 커뮤니티";
+            include "../include/header.php";
+        ?>
+  
+    <!-- //header -->
+
+
+
+
+    <div class="bg">
+        <main id="board_contents">
+            <section id="mainCont">
+            <h2 class="ir_so">메인 컨텐츠</h2>
+            <article class="content-article">
+                <div class="boardType">
+                    <div class="boardWrap">
+                    <div class="board-menu">
+                        <div class="menu-table">
+                            <ul>
+                                <li>명상</li>
+                                <li>자연소리</li>
+                                <li>백색소음</li>
+                                <li>무드</li>
+                            </ul>
+                        </div> 
+                    </div>
+                    <div class="board-play">
+                        <div class="play-cont">
+                            <em>명상</em>
+                            <span><div class="play-but">플레이버튼</div></span>                            
+                        </div>
+                        <div class="play-list">
+                            <div class="play-list01">play-list01</div>
+                            <!-- //play-list01 end -->
+                            <div class="play-list02">play-list02</div>
+                            <!-- //play-list02 end -->
+                            <div class="play-list03">play-list03</div>
+                            <!-- //play-list03 end -->
+                            <div class="play-list04">play-list04</div>
+                            <!-- //play-list04 end -->
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </article>
+            </div>
+        </div>
+    </div>
+    <!-- //contents -->
+
+
+<script>
+
+
+</script>
+</div>
+
+</body>
+</html>
