f4baace045c8eef1303bc4e038fe745478c184c5
kimtaehwi84 <87642334+wow2002r@users.noreply.github.com>
2021-10-15 16:48:17 +0900

    Create head.php

---
+++ TEAM-WORK/goodnight/include/head.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?=$title?></title>
+
+    <link rel="stylesheet" href="/goodnight/asset/css/reset.css">
+    <link rel="stylesheet" href="/goodnight/asset/css/fonts.css">
+    <link rel="stylesheet" href="/goodnight/asset/css/style.css">
+    <script src="https://kit.fontawesome.com/9a767740aa.js" crossorigin="anonymous"></script>
+</head>
